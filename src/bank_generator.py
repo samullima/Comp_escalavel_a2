a87a3b19524477e09c3e81baf22f6c326e0e1bab
KaikyBraga <kaiky.braga06221607@gmail.com>
2025-04-19 16:31:53 -0300

    feat : adicionei o sqlite

--- src/bank_generator.py
+++ src/bank_generator.py
@@ -6,69 +6,131 @@ import sqlite3
 
 fake = Faker()
 
-def gerar_contas(n_accounts=1000):
+def gerar_contas(n_accounts, n_clients):
     status_conta = ["pendente", "bloqueada", "desbloqueada"]
     account_type_choices = ["corrente", "salário", "poupança"]
+    
+    # Lista de possíveis locais
+    locais = [
+        "Rio Branco", "Maceió", "Macapá", "Manaus", "Salvador", "Fortaleza", "Brasília",
+        "Vitória", "Goiânia", "São Luís", "Cuiabá", "Campo Grande", "Belo Horizonte",
+        "Belém", "João Pessoa", "Curitiba", "Recife", "Teresina", "Rio de Janeiro",
+        "Natal", "Porto Alegre", "Porto Velho", "Boa Vista", "Florianópolis",
+        "São Paulo", "Aracaju", "Palmas"
+    ]
+    
+    dados = []
+    account_id_counter = 0  
+
+    for i in range(n_clients): 
+        num_contas_cliente = random.randint(1, 3)  
+        cliente_local = random.choice(locais) 
+        
+        for _ in range(num_contas_cliente):
+            if account_id_counter >= n_accounts:
+                break  
+ 
+            account_id = account_id_counter
+            account_id_counter += 1  
+
+            current_balance = round(random.uniform(0.0, 15000.0), 2)
+            account_type = random.choice(account_type_choices)
+            opening_date = fake.date_time_this_year()
+            account_status = random.choice(status_conta)
+
+            dados.append({
+                "customer_id": i, 
+                "account_id": account_id,
+                "current_balance": current_balance,     
+                "account_type": account_type,       
+                "opening_date": opening_date.date(),
+                "account_status": account_status,
+                "account_location": cliente_local
+            })
+
+    return pd.DataFrame(dados)
+
+
+def gerar_clientes(n_clients):
     dados = []
 
-    for i in range(n_accounts): 
-        customer_id = i  
-        account_id = i  
-        current_balance = round(random.uniform(0.0, 15000.0), 2)
-        account_type = random.choice(account_type_choices)
-        opening_date = fake.date_time_this_year()
-        account_status = random.choice(status_conta)
+    for i in range(n_clients+1):
+        name = fake.name()
+        email = fake.email()
+        address = fake.address().replace("\n", " - ")
+        phone = int(fake.msisdn()[0:11])  
 
         dados.append({
-            "customer_id": customer_id,
-            "account_id": account_id,
-            "current_balance": current_balance,
-            "account_type": account_type,
-            "opening_date": opening_date,
-            "account_status": account_status
+            "customer_id": i,
+            "name": name,
+            "email": email,
+            "address": address,
+            "phone": phone
         })
 
     return pd.DataFrame(dados)
 
 
-def gerar_transacoes(n_transactions=100000, n_accounts=1000):
+def gerar_transacoes(df_contas, n_transactions, n_accounts):
     tipos_transacoes = ["depósito", "retirada", "transferência", "pagamento"]
     dados = []
+    
+    locais = [
+        "Rio Branco", "Maceió", "Macapá", "Manaus", "Salvador", "Fortaleza", "Brasília",
+        "Vitória", "Goiânia", "São Luís", "Cuiabá", "Campo Grande", "Belo Horizonte",
+        "Belém", "João Pessoa", "Curitiba", "Recife", "Teresina", "Rio de Janeiro",
+        "Natal", "Porto Alegre", "Porto Velho", "Boa Vista", "Florianópolis",
+        "São Paulo", "Aracaju", "Palmas"
+    ]
 
+    col_cidade = df_contas["account_location"]
+    
     for i in range(n_transactions):
-        transation_id = i 
-        account_id = random.randint(0, n_accounts)
-        recipient_id = random.randint(0, n_accounts)
+        account_id = random.randint(0, n_accounts - 1) 
+        recipient_id = random.randint(0, n_accounts - 1)
         amount = round(random.uniform(10.0, 700.0), 2)
         tipo = random.choice(tipos_transacoes)
 
-        # Geração de horários
         time_start = fake.date_time_this_year()
-        processed_at = time_start + timedelta(seconds=random.randint(5, 60))
-        
-        location = fake.city()
+        time_end = time_start + timedelta(seconds=random.randint(5, 60))
+
+        transaction_date = time_start.date()
+
+        if random.random() < 0.98:
+            # Cidade Padrão
+            location = col_cidade.iloc[account_id]
+        else:
+            # Suspeita
+            location = random.choice(locais)
 
         dados.append({
-            "transation_id": transation_id,
+            "transation_id": i,
             "account_id": account_id,
             "recipient_id": recipient_id,
             "amount": amount,
             "type": tipo,
-            "time_start": time_start.time(),
             "location": location,
-            "processed_at": processed_at.time()
+            "time_start": time_start.time(),
+            "time_end": time_end.time(),
+            "date": transaction_date
         })
 
-    return pd.DataFrame(dados)
+    return pd.DataFrame(dados)              
 
-n_transacoes = 100000
-n_contas = 1000
+# ALTERAR ESSES CAMPOS:
+n_transacoes = 100000   
+n_contas = 1000         
+n_max_clientes = 500      
 connection_obj = sqlite3.connect('data/data.db')
 
-df_contas = gerar_contas(n_contas)
-df_contas.to_csv("accounts.csv", index=False)
-df_contas.to_sql('accounts', connection_obj, if_exists='replace', index=False)
+df_contas = gerar_contas(n_contas, n_max_clientes)
+df_customers = gerar_clientes(df_contas["customer_id"].iloc[-1])
+df_transactions = gerar_transacoes(df_contas, n_transacoes, n_contas)
 
-df_transacoes = gerar_transacoes(n_transacoes, n_contas)
-df_transacoes.to_csv("transactions.csv", index=False)
-df_transacoes.to_sql('transactions', connection_obj, if_exists='replace', index=False)
+df_contas.to_sql('accounts', connection_obj, if_exists='replace', index=False)
+df_customers.to_sql('customers', connection_obj, if_exists='replace', index=False)
+df_transactions.to_sql('transactions', connection_obj, if_exists='replace', index=False)
+
+df_contas.to_csv("accounts.csv", index=False)
+df_customers.to_csv("customers.csv", index=False)
+df_transactions.to_csv("transactions.csv", index=False)
