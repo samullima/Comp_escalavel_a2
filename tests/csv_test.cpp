1d13933d4b08672690b91a4b668f797970db66d5
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2025-04-22 03:31:52 -0300

    cleanup : removidas funções bah

--- tests/csv_test.cpp
+++ tests/csv_test.cpp
@@ -50,28 +50,3 @@ int main(int argc, char* argv[]) {
     benchmarkingCSV(thread::hardware_concurrency());
     return 0;
 }
-
-int bah(int argc, char* argv[]) {
-    // Verifica se o número de threads foi passado como argumento
-    if (argc > 1) {
-        MAX_NUM_THREADS = atoi(argv[1]);
-        if (MAX_NUM_THREADS < 2) {
-            cout << "Número de threads deve ser maior ou igual a 2. Usando 2 threads." << endl;
-            MAX_NUM_THREADS = 2;
-        }
-    }
-    cout << "Número de threads máximo: " << MAX_NUM_THREADS << endl;
-    ThreadPool pool(MAX_NUM_THREADS);
-
-    // Nome do arquivo CSV
-    string filename = "data/transactions/transactions";    
-
-    vector<string> colTypes = {"int", "int", "int", "float", "string", "string", "string", "string"};
-    DataFrame * df = readCSV(1, filename, MAX_NUM_THREADS, colTypes, pool);
-    // df->printDF();
-    cout << "Deu certo" << endl;
-    cout << "Número de colunas: " << df->numCols << endl;
-    cout << "Número de registros: " << df->numRecords << endl;
-
-    return 0;
-}
