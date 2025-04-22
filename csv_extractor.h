b999739116afa5bd68791a538bb37c7fe72770c8
KaikyBraga <kaiky.braga06221607@gmail.com>
2025-04-21 22:41:27 -0300

    del : deletei lixo

--- csv_extractor.h
+++
@@ -1,4 +0,0 @@
-
-void readCSVLines(ifstream& file, vector<string>& linesRead, bool& fileAlreadyRead, mutex& mtxFile);
-void processCSVLines(const vector<string>& linesRead, DataFrame* df, int& recordsCount, bool& fileAlreadyRead, mutex& mtxFile, mutex& mtxCounter);
-DataFrame* readCSV(const string& filename, int numThreads);
