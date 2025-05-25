88d1f5b390a1c53db05d0094481b31123a612c93
AlessandraBello <alessandrabellosoares@gmail.com>
2025-05-25 12:17:34 -0300

    path

--- grpc/test/client_abnormal.py
+++ grpc/test/client_abnormal.py
@@ -1,6 +1,10 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from grpc_client import GRPCClient
 
-client = GRPCClient()
+client = GRPCClient("10.22.171.206:9999")
 def test_abnormal_transactions():
     abnormal_transactions = client.abnormal_transactions()
 
--- grpc/test/client_transactionsInfo.py
+++ grpc/test/client_transactionsInfo.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from grpc_client import GRPCClient
 
 client = GRPCClient()
